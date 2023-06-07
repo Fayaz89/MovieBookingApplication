122bbe037306b1c761659205b382c7317b855e64
Fayaz89 <83117029+Fayaz89@users.noreply.github.com>
2023-06-07 18:18:51 +0530

    Add files via upload

---
+++ Backend/src/main/java/com/moviebookingapp/repository/MovieRepository.java
@@ -0,0 +1,21 @@
+package com.moviebookingapp.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.moviebookingapp.models.Movie;
+
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie,String> {
+
+    List<Movie> findByMovieNameStartingWith(String movieName);
+    
+    List<Movie> findByMovieName(String movieName);
+
+    Movie findByMovieNameAndTheatreName(String moviename,String theatreName);
+
+    void deleteByMovieName(String movieName);
+}
+
